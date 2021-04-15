81177ee346cb7feb09c1517f85ebec64d51bd59f
Alexand00r <otpushchenko@yandex.ru>
2021-04-16 01:15:36 +0300

    06_07_star_db_base_structure

--- 06_star-db/src/components/app/app.js
+++ 06_star-db/src/components/app/app.js
@@ -11,7 +11,7 @@ export default class App extends Component {
 
     render() {
         return (
-            <div>
+            <div className="star-db-app">
                 <Header />
                 <RandomPlanet />
 
