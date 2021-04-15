105b2ad4d4dee34aeb9885e15dd146e492cf77f0
Alexand00r <otpushchenko@yandex.ru>
2021-04-16 00:15:20 +0300

    06_05_fetch_catch_errors

--- 06_star-db/src/index.js
+++ 06_star-db/src/index.js
@@ -1,4 +1,19 @@
-fetch('http://swapi.dev/api/people/1/')
-.then((res) => {
-    console.log('Got response', res.status, res.json());
-})
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if (!res.ok){
+        throw new Error(`Could not fetch ${url}` + res.status)
+    }
+
+    const body = await res.json();
+    return body;
+};
+
+getResource('http://swapi.dev/api/people/1324234/')
+    .then((body) => {
+        console.log(body);
+    })
+    .catch((err) => {
+        console.error('Could not catch', err);
+    });
